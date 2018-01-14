9fd9a44cc8933b17ce4dcd1d24fc8673fed807f8
Jeremie <jeremie.langevin@outlook.com>
2018-01-13 19:18:00 -0500

    ajax.playlist.plush.sidebar.js: remove class saved on click saved-in-list

--- includes/js/ajax.playlist.plush.sidebar.js
+++ includes/js/ajax.playlist.plush.sidebar.js
@@ -97,17 +97,16 @@ function  ajax_playlist_remove_sidebar_btn($, object_id)  {
                 dataType: 'JSON',
                 success: function(data){
                     //   console.log(data);
-                /*
-                postdata = $("#album-class-artist-list-id-"+data+" a").toggleClass('saved');
+                    
+                    /* postdata = $("#album-class-artist-list-id-"+data+" a").toggleClass('saved');
 
                     postdata; */
 
-                    postdata_ = $("#postid-"+data+" a").toggleClass('saved');
-                    
-                                        postdata_;
-                      
+                    $("#postid-"+data+" a").removeClass('saved');
 
-                        },
+                    $("#album-class-artist-list-id-"+data+" a").removeClass('saved');
+
+                },
                 error: function(errorThrown){
                     //error stuff here
                 }
