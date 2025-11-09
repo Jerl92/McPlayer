34fb7862b0d226e21828946b91062da881511a1e
Jérémie Langevin <jeremie.langevin@outlook.com>
2025-11-09 09:12:03 -0500

    Update BASE_URL for McPlayer plugin

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Base URL of the application
-define('BASE_URL','https://'. $_SERVER['HTTP_HOST'].'/wp-content/plugins/file-manager/includes/');
+define('BASE_URL','https://'. $_SERVER['HTTP_HOST'].'/wp-content/plugins/McPlayer/includes/');
 
 // Path of the download-link.php file
 define('DOWNLOAD_PATH', BASE_URL.'download.php');
