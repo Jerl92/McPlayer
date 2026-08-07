71f18fe0796704b53b94b9ea5a8225b636744c49
Jérémie Langevin <jere.langevin@outlook.com>
2026-08-07 18:06:03 -0400

    Implement score input validation and AJAX save
    Added validation for score input to ensure it is between 0 and 5, with error handling for invalid values.

--- includes/js/ajax.save.score.js
+++ includes/js/ajax.save.score.js
@@ -5,27 +5,49 @@ function save_score($){
         clearTimeout(typingTimer);
         var input = jQuery(this);
         typingTimer = setTimeout(function() {
-            var finalNumber = input.val();
-            var postid = jQuery('.postid').html();
-            var userid = jQuery('.userid').html();
-            jQuery.ajax({
-                type: 'post',
-                url: save_score_ajax_url,
-                data: {
-                    'userid':userid,
-                    'postid': postid,
-    		        'value': finalNumber,
-                    'action': 'save_score'
-                },
-                dataType: 'json',
-                success: function(data){
-    		        jQuery('.newarray').html(data);
-                },
-                error: function(errorThrown){
-                    console.log(errorThrown);
-                }
-            });
+        	
+        var error = 0;
+		var finalNumber = input.val();
+		var postid = jQuery('.postid').html();
+		var userid = jQuery('.userid').html();
+		
+		if(input.val() > 5){	
+			error = 1;
+			jQuery('.song-unique-score-input').css('border', '0.5px solid red');
+			jQuery('.newarray').html('Value need to be under 5.');
+		}
+		
+		if(input.val() < 0){	
+			error = 1;
+			jQuery('.song-unique-score-input').css('border', '0.5px solid red');
+			jQuery('.newarray').html('Value need to be below 0.');
+		}
+		
+		if(input.val() > 0 && input.val() < 5){	
+			error = 0;
+			jQuery('.song-unique-score-input').css('border', '0.5px solid gray');
+		}
             
+        if(error === 0){
+			jQuery.ajax({
+				type: 'post',
+				url: save_score_ajax_url,
+				data: {
+				    'userid':userid,
+				    'postid': postid,
+				    'value': finalNumber,
+				    'action': 'save_score'
+				},
+				dataType: 'json',
+				success: function(data){
+				        jQuery('.newarray').html(data);
+				},
+				error: function(errorThrown){
+				    console.log(errorThrown);
+				}
+			});
+		}
+		
         }, doneTypingInterval);
     });
 }
