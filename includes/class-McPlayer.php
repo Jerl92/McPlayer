47f974a23dc03b9ab592c94c451d36be27d6658c
Jérémie Langevin <jere.langevin@outlook.com>
2026-08-07 22:20:55 -0400

    Update to version 2.3

--- includes/class-McPlayer.php
+++ includes/class-McPlayer.php
@@ -69,7 +69,7 @@ class Mcplayer {
 	public function __construct() {
 
 		$this->plugin_name = 'McPlayer';
-		$this->version = '1.8';
+		$this->version = '2.3';
 
 		$this->load_dependencies();
 		$this->set_locale();
